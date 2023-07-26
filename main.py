3008e8beb30bb086ba3026e878339a6dcbe871e2
Daniil <104896330+menschee@users.noreply.github.com>
2023-07-26 22:09:15 +0300

    small improvements

--- main.py
+++ main.py
@@ -4,20 +4,18 @@ from telebot import types
 API_TOKEN = '6407889799:AAHKALxy5JoajOLUV-LnK5mPx8SJJXJwcAU'
 bot = telebot.TeleBot(API_TOKEN)
 
-# Function to handle inline query results
 @bot.inline_handler(lambda query: query.query.lower() == 'toncoin')
 def query_text(inline_query):
     try:
-        # Image url
         image_url = 'https://github.com/menschee/schee.men_bot/blob/main/qr.png?raw=true'
 
-        # Creating Inline keyboard markup (two buttons with URLs)
         keyboard = types.InlineKeyboardMarkup()
         btn1 = types.InlineKeyboardButton('Tonkeeper', url='https://app.tonkeeper.com/transfer/EQCM2ReRAEkCGaAM33ZUx-_vAj0qPgI5TeBQtIp1tjjFe1aa')
         btn2 = types.InlineKeyboardButton('Tonhub', url='https://tonhub.com/transfer/EQCM2ReRAEkCGaAM33ZUx-_vAj0qPgI5TeBQtIp1tjjFe1aa?amount=1000000000')
-        keyboard.add(btn1, btn2)
+        btn3 = types.InlineKeyboardButton('TON Wallet', url='ton://transfer/EQCM2ReRAEkCGaAM33ZUx-_vAj0qPgI5TeBQtIp1tjjFe1aa')
+        keyboard.row(btn1, btn2)
+        keyboard.row(btn3)
 
-        # Creating text with the image and keyboard
         photo = types.InlineQueryResultPhoto(
             id='1',
             photo_url=image_url,
@@ -27,9 +25,22 @@ def query_text(inline_query):
             parse_mode='MarkdownV2'
         )
 
-        # Answering inline query with the created article
         bot.answer_inline_query(inline_query.id, [photo])
 
+    except Exception as e:
+        print(str(e))
+
+@bot.inline_handler(lambda query: 'bio' == query.query.lower())
+def query_text(inline_query):
+    try:
+        r = types.InlineQueryResultArticle(
+                id='2',
+                title="Send @menschee's bio",
+                input_message_content=types.InputTextMessageContent(
+                    message_text="===Socials==\n\nTelegram: @menschee\nTwitter: @menscheee\n\n*bold\==Channels==The Daily TON: @thedailyton\nThe Daily TON RUS: @thedailytonrus"
+                )
+            )
+        bot.answer_inline_query(inline_query.id, [r])
     except Exception as e:
         print(str(e))
 
